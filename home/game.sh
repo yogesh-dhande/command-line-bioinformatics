ebbaeddb512f439f15ea47939ffff7d59cb228b0
Yogesh Dhande <yogeschkd1988@yahoo.com>
2020-09-14 04:51:19 +0000

    Reverted CSV input in game.sh

--- home/game.sh
+++ home/game.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-IFS=","
-while read PROMPT EXPECTED_COMMAND; do
-    while :
-        do
-            # PROMPT='Type a command'
-            # EXPECTED_COMMAND='grep docker sandbox.sh'
+while :
+    do
+        PROMPT='Type a command'
+        EXPECTED_COMMAND='grep docker sandbox.sh'
 
-            echo ${PROMPT}; echo 
+        echo ${PROMPT}; echo 
 
-            read COMMAND
+        read COMMAND
 
-            if diff -q <(${COMMAND}) <(${EXPECTED_COMMAND}) > /dev/null
-            then 
-                echo .......
-                echo $(${COMMAND})
-                echo .......
-                break
-            else 
-                echo Please try again ...
-            fi
-            echo
-        done
-done < home/data.csv
+        if diff -q <(${COMMAND}) <(${EXPECTED_COMMAND}) > /dev/null
+        then 
+            echo .......
+            echo $(${COMMAND})
+            echo .......
+            break
+        else 
+            echo Please try again ...
+        fi
+        echo
+    done
